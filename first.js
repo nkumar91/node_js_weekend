66336e93ff163cda89a2833af6b12c2f9b7061b8
Nishant <nknishant91@gmail.com>
2023-04-30 09:56:51 +0530

    second_commit

---
+++ first.js
@@ -0,0 +1,30 @@
+// console.log("A")
+// console.log("B")
+// setTimeout(()=>{
+// console.log("D")
+// },5000)
+
+// setTimeout(()=>{
+//     console.log("D")
+//     },10000)
+// console.log("C")
+
+
+// console.log("A")
+
+// setTimeout(()=>{
+// console.log("B")
+// },0)
+
+// setImmediate(() => {
+//     console.log("G");
+// });
+
+
+// process.nextTick(()=>{
+//  console.log("M")   
+// })
+
+// console.log("C")
+
+
