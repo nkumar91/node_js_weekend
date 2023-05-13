cc15effcfa462269cc7d1726ea4cab2318fbf621
Nishant <nknishant91@gmail.com>
2023-05-13 10:10:04 +0530

    data receved

--- app.js
+++ app.js
@@ -1,24 +1,25 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const multer = require('multer')
 app.use(cors())
 
-app.get("/data",function(request,response){
+// app.get("/data",function(request,response){
   
-    response.json({
-        status:"success"
-    })
-})
+//     response.json({
+//         status:"success"
+//     })
+// })
 
-app.get("/jodo",function(request,response){
+// app.get("/jodo",function(request,response){
 
-    const data = request.query
-    console.log(data)
-    response.json({
-        message:"Jod Diya",
-        yourData:data.name+ " "+data.email+" hello"
-    })
-})
+//     const data = request.query
+//     console.log(data)
+//     response.json({
+//         message:"Jod Diya",
+//         yourData:data.name+ " "+data.email+" hello"
+//     })
+// })
 
 // app.post("/data",function(request,response){
 //     response.json({
@@ -35,6 +36,38 @@ app.get("/jodo",function(request,response){
 //         status:"Delete Karo bhai"
 //     })
 // })
+
+ // middleware  (application level middleware , built middleware,erorr middleware,route based middleware)
+//app.use('/signup',express.json()) 
+app.use(express.json()) 
+app.use(express.urlencoded({extended:true}))
+app.use(multer().none())
+app.post("/signup",function(request,response){
+    const rdata = request.body
+    response.json({data:rdata})
+})
+
+app.post("/signupp",function(request,response){
+    const rdata = request.body
+    response.json({data:rdata})
+})
+
+app.post("/signup/:id/:my",function(request,response){
+    const x = request.params.my
+    response.json({data:x})
+})
+
+app.post("/urlencode",function(request,response){
+    const data = request.body
+    response.json({data:data})
+})
+
+app.post("/formdata", function(request,response){
+    const data = request.body
+    response.json({data:data})
+})
+
+
 
 
 module.exports = app
